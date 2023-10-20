f356b58425e3d6c051b3895ff67758d5a0ef78b7
Lee <cleberli.rocha@gmail.com>
2023-10-20 18:32:57 -0300

    Adicionando Vuetify e VueTheMask ao projeto

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,21 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+const vuetify = new Vuetify()
+
+import VueTheMask from 'vue-the-mask'
+Vue.use(VueTheMask)
+
+import api from './provider/api'
 
 Vue.config.productionTip = false
+Vue.prototype.$api = api
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app')
