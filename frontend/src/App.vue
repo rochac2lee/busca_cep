4ad8e4fe2b29ae3190facbcb41d797a1a14626bd
Lee <cleberli.rocha@gmail.com>
2023-10-20 20:23:53 -0300

    Adicionando snackbar para mostrar sucesso e erro amigável

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,38 @@
 <template>
   <v-app>
+    <v-snackbar
+      v-model="snackbarOpen"
+      :color="snackbarColor"
+      :timeout="snackbarTimeout"
+      height="60"
+      fixed
+      bottom
+    >
+      {{ snackbarText }}
+    </v-snackbar>
     <router-view />
   </v-app>
 </template>
 
 <script>
+import { eventbus } from "@/main.js";
+
 export default {
-  name: "App"
+  name: "App",
+  data: () => ({
+    snackbarOpen: false,
+    snackbarColor: "success",
+    snackbarText: "",
+    snackbarTimeout: 3000
+  }),
+  mounted() {
+    eventbus.$on("toggleSnackbar", snackbarObj => {
+      this.snackbarOpen = snackbarObj.show;
+      this.snackbarColor = snackbarObj.color;
+      this.snackbarText = snackbarObj.text;
+      this.snackbarTimeout = snackbarObj.timeout;
+    });
+  }
 };
 </script>
 
@@ -15,8 +41,7 @@ export default {
   font-family: "Rb", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
