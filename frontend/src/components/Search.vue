89ce7396f3916bdf801a4a16059fc5b2b207188c
Lee <cleberli.rocha@gmail.com>
2023-10-20 18:33:19 -0300

    Inserindo input de cep no formulário

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-container>
+      <v-form @submit.prevent="search">
+        <v-row>
+          <v-col cols="12" md="2">
+            <v-text-field
+              v-model="params.cep"
+              label="CEP"
+              color="primary"
+              outlined
+              rounded
+              v-mask="'#####-###'"
+            />
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      params: {}
+    };
+  }
+};
+</script>
